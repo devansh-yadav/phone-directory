9f14d2fde54cac432f5a4efc8df6af2d5d55474f
Devansh Yadav <devansh24yadav@gmail.com>
2021-07-08 14:42:13 +0530

    added state in 'AddSubscriber' and 'App' components

--- src/App.js
+++ src/App.js
@@ -1,50 +1,63 @@
-import React from "react";
+import React, { Component } from 'react';
 import Header from "./Header";
 import './App.css';
+import './common/common.css'
 
-function App() {
+// let subscribers = [
+//   {
+//     id: 1,
+//     name: "Shilpa",
+//     phone: "8888888888"
+//   },
+//   {
+//     id: 2,
+//     name: "Srishti",
+//     phone: "9999999999"
+//   }
+// ]
 
-  let subscribers = [
-    {
-      id: 1,
-      name: "Shilpa",
-      phone: "8888888888"
-    },
-    {
-      id: 2,
-      name: "Srishti",
-      phone: "9999999999"
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      subscribersListToShow: []
     }
-  ]
+  }
 
-  return (
-    <div>
-      <Header heading = "Phone Directory"/>
+  render() {
 
-      <div className="body-container">
 
-        <button className="custom-btn add-btn">Add</button>
+    return (
+      <div>
+        <Header heading="Phone Directory" />
 
-        <div className="grid-container heading-container">
-          <span>Name</span>
-          <span>Phone</span>
-        </div>        
-        
-        {
-          subscribers.map(sub => {
-            return <div key={sub.id} className="grid-container">
-              <span className="grid-item">{sub.name}</span>
-              <span className="grid-item">{sub.phone}</span>
-              <span className="grid-item">
-                <button className="custom-btn delete-btn">Delete</button>
-              </span>
-            </div>
-          })
-        }
+        <div className="body-container">
 
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span>Name</span>
+            <span>Phone</span>
+          </div>
+
+          {
+            this.state.subscribersListToShow.map(sub => {
+              return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+                <span className="grid-item">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+              </div>
+            })
+          }
+
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
